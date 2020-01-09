caddec38ebfc25aa622efee843ba164edb8d7c69
abhi030697 <abhinavpsawarkar@gmail.com>
2020-01-10 01:32:03 +0530

    ToDoList program

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDz88_bHMhEZN_qFtiJL590UsFJVi5STHc",
+    authDomain: "todolistapp-b8166.firebaseapp.com",
+    databaseURL: "https://todolistapp-b8166.firebaseio.com",
+    projectId: "todolistapp-b8166",
+    storageBucket: "todolistapp-b8166.appspot.com",
+    messagingSenderId: "504449516282",
+    appId: "1:504449516282:web:534571970319e42a98ff43",
+    measurementId: "G-QGV2L2WKJY"
+  }
 };
